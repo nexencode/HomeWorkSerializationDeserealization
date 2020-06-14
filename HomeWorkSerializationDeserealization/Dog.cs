53ea21918faf38e15a7e37f852f598d41552b85f
Nemanja Nikolć <58153223+nexencode@users.noreply.github.com>
2020-06-14 20:26:28 +0200

    Add project files.

---
+++ HomeWorkSerializationDeserealization/Dog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkSerializationDeserealization
+{
+    [Serializable]
+    public class Dog
+    {
+        #region Fields and Properties
+        private static int id;
+        public int ID { get; set; } = ++id;
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+
+        public string DogBreed { get; set; }
+        #endregion
+
+        #region Constructors
+        public Dog() { }
+
+        public Dog(string name, string color, int age, string dogBreed)
+        {
+            this.Name = name;
+            this.Color = color;
+            this.Age = age;
+            this.DogBreed = dogBreed;
+        }
+        #endregion
+
+        #region Methods
+        public void PrintDog()
+        {
+            Console.WriteLine($"Id: {ID}, Name: {Name}, Dog Breed: {DogBreed}, Color: {Color}, Age: {Age}.");
+        }
+        #endregion
+    }
+}
---
+++ HomeWorkSerializationDeserealization/DogRepository.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkSerializationDeserealization
+{
+    public static class DogRepository
+    {
+        const string dogFilePath = @"C:\Users\nexen\source\repos\HomeWorkSerializationDeserealization\dogs.json";
+
+        /// <summary>
+        /// Add new dogs to List and Serialize file .
+        /// </summary>
+        public static void AddAndSerializeDogs()
+        {
+            var allDogs = new List<Dog>()
+            {
+                new Dog("Dante", "Yellow", 12, "Labrador Retriver"),
+                new Dog("Lara", "White", 10, "Gold Retriver")
+            };
+
+            AddNewDog(allDogs);
+
+            File.WriteAllText(dogFilePath, JsonConvert.SerializeObject(allDogs));
+        }
+
+        /// <summary>
+        /// User Add new dog from console input.
+        /// </summary>
+        public static void AddNewDog(List<Dog> listOfDogs)
+        {
+            Console.WriteLine("Press 'Y' to add new dog or press 'Q' to exit from application: ");
+
+            if (Console.ReadLine().ToLower() == "y")
+            {
+                #region User Input
+                Console.WriteLine("Enter dog name: ");
+                var dogName = Console.ReadLine();
+
+                Console.WriteLine("Enter dog color: ");
+                var dogColor = Console.ReadLine();
+
+                Console.WriteLine("Enter dog age: ");
+                var dogAge = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter dog Breed: ");
+                var dogBreed = Console.ReadLine();
+                #endregion
+
+                Dog newDog = new Dog(dogName, dogColor, dogAge, dogBreed);
+
+                listOfDogs.Add(newDog);
+
+                AddNewDog(listOfDogs);
+            }
+            else
+            {
+                Console.WriteLine("You are exiting app...");
+            }
+
+        }
+
+        /// <summary>
+        /// Deserialize dogs from file and print to console.
+        /// </summary>
+        public static void DeserializeAndPrintAllDogs()
+        {
+            List<Dog> dogsFromFile = JsonConvert.DeserializeObject<List<Dog>>(File.ReadAllText(dogFilePath));
+
+            foreach (Dog dog in dogsFromFile)
+            {
+                dog.PrintDog();
+            }
+        }
+    }
+}
